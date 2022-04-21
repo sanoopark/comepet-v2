f63d8ab746087e2296ba0bd60bde80811f2a7278
sanoopark <sanoopark@gmail.com>
2022-04-21 20:18:59 +0900

    Chore: 프로젝트 초기설정

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>ComePet</title>
+        <meta
+          name="description"
+          content="반려동물 실종신고 및 유기동물 입양 서비스"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main></main>
+      <footer></footer>
+    </>
+  );
+};
+
+export default Home;
